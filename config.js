da5bae0c2e9669b6fffbec58a527bd77e9e582cf
aTNa <atay.narynov@gmail.com>
2020-08-22 12:40:28 +0600

    test config

---
+++ config.js
@@ -0,0 +1,26 @@
+form_config = {
+    main_title: "OPENING PROCEDURES",
+    questions: [
+        {
+            title: "Group number:",
+            type: "select",
+            answer: [
+                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6", "Group 7", "Group 8", "Group 9", "Group 10"
+            ]
+        },
+        {
+            title: "PS ID:",
+            type: "input"
+        },
+        {
+            title: "Time of arrival:",
+            type: "time"
+        },
+        {
+            title: "PSO chairperson male/female:",
+            type: "radio",
+            answer: ["Male", "Female"]
+        }
+    ],
+    gateway: "www.blablabla.kg"
+}
